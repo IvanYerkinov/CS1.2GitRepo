548121f940f535a0b6d808f342be3c60dfdf1bd1
Ivan Yerkinov <darkblackvoid@yahoo.com>
2020-01-28 10:22:20 -0800

    Finished tutorial step 3

---
+++ sourcetext.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def histogram(longstring):
+    string = longstring.split(" ")
+    di = dict()
+
+    for st in string:
+        if st in di:
+            di[st] += 1
+        else:
+            di[st] = 1
+
+    return di
+
+
+def unique_words(hist):
+    i = 0
+    for key in hist:
+        i += 1
+    return i
+
+
+def frequency(hist, word):
+    return hist[word]
+
+
+def get_file(filename):
+    with open(filename, 'r') as f:
+        data = f.read().replace("\n", "")
+    return data
+
+
+if __name__ == "__main__":
+    his = histogram(get_file(sys.argv[1]))
+    print(unique_words(his))
+    print(frequency(his, "and"))
+    print(his)
